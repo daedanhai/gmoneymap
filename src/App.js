bcd3b7874eb7ed45ca240bd4096d7cb8c569f51d
daedanhai <daedanhai@naver.com>
2023-02-01 18:53:36 +0900

    '230201'

--- src/App.js
+++ src/App.js
@@ -1,64 +1,111 @@
-import logo from './logo.svg';
 import axios from 'axios'
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
-const { kakao } = window;
-// 이때, 아래의 사진처럼 kakao.maps 부분에 'kakao' is not undefined 오류가 나는 것을 발견할 수 있습니다.
-//이유는 스크립트로 kakao maps api를 심어서 가져오면 window전역 객체에 들어가게 됩니다.
-//그런데 함수형 컴포넌트에서는 이를 바로 인식하지 못한다고 합니다.
-//그렇기 때문에 코드 상단에 const { kakao } = window를 작성하여 함수형 컴포넌트에 인지 시키고 window에서 kakao객체를 뽑아서 사용하면 됩니다.
+import { useEffect, useMemo, useState } from 'react';
+import { Map, MapMarker, MarkerClusterer ,useMap} from 'react-kakao-maps-sdk';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
-  const url = 'https://api.odcloud.kr/api/15036011/v1/uddi:fc22c9fb-b27b-4693-9563-dd67097eb5c3?page=1&perPage=1000&serviceKey=620gIHWFqC%2FtXHpl8VcnOnfSQG3WaN5lQ4h4pxCK4dwjQr1QLlVOt0sopW186ChnXGVyxhZE%2FNADigXJrrXV4g%3D%3D'
-  //재귀함수로 다 돌려서 페이지를 늘려서 다 받아온다.
-
-  let [arr,setArr] = useState([]);
-  let [points,setPoint] = useState([]);
-  let mapRef = useRef(null);
-
+  const dataUrl = 'https://api.odcloud.kr/api/15036011/v1/uddi:fc22c9fb-b27b-4693-9563-dd67097eb5c3?page=1&perPage=300&serviceKey=620gIHWFqC%2FtXHpl8VcnOnfSQG3WaN5lQ4h4pxCK4dwjQr1QLlVOt0sopW186ChnXGVyxhZE%2FNADigXJrrXV4g%3D%3D';
+  let [ dataArr,setDataArr ] = useState([]);
+  // let [ filtedData,setFiltedData ] = useState([]);
+  
   useEffect(()=>{
-    const container = document.getElementById('map-area');
-    const options = {
-      center : new kakao.maps.LatLng(37.7387295, 127.0458908),
-      level : 3,
-    }
-    mapRef.current = new kakao.maps.Map(container, options);
-    getGmoneyData();
+    axios.get(dataUrl)
+      .then((response) => {
+        setDataArr(response.data.data);
+      })
+      .catch((error)=> {
+        console.log(error.data);
+      });
   },[]);
+  console.log(dataArr);
 
-  useEffect(()=>{
-    pointsInit();
-  },[arr]);
 
-  useEffect(()=>{
-    var i, marker;
-    for (i = 0; i < points.length; i++) {
-      // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
-      marker = new kakao.maps.Marker({ position : points[i] });
-      marker.setMap(mapRef.current);
-    }
-  },[points])
- 
-  const getGmoneyData = () => {
-    axios.get(url)
-      .then((Response) => {
-        setArr(Response.data.data);
-      })
-      .catch((Error)=>{
-        console.log(Error);
-      })
-  }
+  // useEffect(()=>{
+  //   dataFilted();
+  // },[dataArr]);
 
-  function pointsInit(){
-    setPoint(arr.map( el => {
-      return new kakao.maps.LatLng(el['위도'], el['경도'])
-    })
+
+  const dataFilted = ()=>{
+    return(
+      setFiltedData(
+        dataArr.map((el)=>{
+          return (
+            {
+              name: el['상호명'],
+              location: el['소재지(도로명주소)'],
+              latlng: { lat: el['위도'], lng: el['경도'] }
+            }
+          )
+        })
+      )
+    )
+  };
+
+  const EventMarkerContainer = ({ lng , lat, name, location }) => {
+    const map = useMap()
+    const [isVisible, setIsVisible] = useState(false);
+    return (
+      <MapMarker
+        position={ {
+          lat: lat,
+          lng: lng,
+        } } // 마커를 표시할 위치
+        // @ts-ignore
+        onClick={ ( marker ) => map.panTo( marker.getPosition() )}
+        onMouseOver={ () => setIsVisible(true) }
+        onMouseOut={ () => setIsVisible(false) }
+      >
+        { 
+          isVisible &&
+          <div className='info-wrap'>
+            <h4>{name}</h4>
+            <p>{location}</p>
+          </div>
+        }
+      </MapMarker>
     )
   }
 
   return (
     <div className="App">
-      <div id='map-area' style={{width:'100%',height:'500px'}}></div>
+      
+      <Routes>
+        <Route path="/" element={
+          <Map // 지도를 표시할 Container
+            center={{
+              // 지도의 중심좌표
+              lat: 37.7387295,
+              lng: 127.0458908,
+            }}
+            style={{
+              // 지도의 크기
+              width: "100%",
+              height: "100vh",
+            }}
+            level={5} // 지도의 확대 레벨
+          >
+            <MarkerClusterer
+              averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+              minLevel={5} // 클러스터 할 최소 지도 레벨
+            >
+            { 
+              dataArr.map((value, idx) => (
+                <EventMarkerContainer
+                  key={ idx }
+                  lat = { value['위도'] }
+                  lng = { value['경도']}
+                  name={ value['상호명'] }
+                  location={ value['소재지(도로명주소)'] }
+                />
+              ))
+            }
+            </MarkerClusterer>
+          </Map>
+        } />
+        <Route path="/test" element={<div className='test'>테스트</div>} />
+      </Routes>
     </div>
   );
 }
